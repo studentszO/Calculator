69a321735957ec521f7a828755437d7b0ddfcf10
szOstudent <169681673+studentszO@users.noreply.github.com>
2024-07-01 22:39:35 +0200

    Add eventListener to all the lists (=buttons) to get the textContent (=value)

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-code
+const buttons = document.querySelector(".calculator");
+let clickedValue = buttons.addEventListener("click", (event) => {
+    let target = event.target;
+    return clickedValue = target.textContent;
+});
+
+buttons.addEventListener("click", () => {
+    console.log(clickedValue)
+})
