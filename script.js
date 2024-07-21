d2502c6afffa7c1da374cf8eadf032c8700550c3
studentszO <169681673+studentszO@users.noreply.github.com>
2024-07-21 14:05:48 +0200

    Change EventListener to get the calculator working as intended, seems OK so far

--- script.js
+++ script.js
@@ -1,26 +1,53 @@
 const buttons = document.querySelector(".calculator");
+const display = document.querySelector(".result-container");
 
 buttons.addEventListener("click", (event) => {
     let target = event.target;
-    if (firstNumber.length >= 0 
-        && [0,1,2,3,4,5,6,7,8,9].includes(Number(target.textContent))
-        && (operator === undefined))
-        return firstNumber += target.textContent;
-    else if (["/", "+", "*", "-"].includes(target.textContent) && (firstNumber.length > 0))
-        return operator = target.textContent;
-    else if (operator !== undefined 
-        && firstNumber.length > 0
-        && [0,1,2,3,4,5,6,7,8,9].includes(Number(target.textContent)))
-        return secondNumber += target.textContent
-    if (target.textContent === "=") {
-        console.log(operate(Number(firstNumber), operator, Number(secondNumber)));
-        clearValues();
+
+
+    if ((display.textContent === "0" || triggerNewContent === 1) && [0,1,2,3,4,5,6,7,8,9].includes(Number(target.textContent))) {
+        display.textContent = "";
+        triggerNewContent = 0;
     }
+
+    if([0,1,2,3,4,5,6,7,8,9].includes(Number(target.textContent)))
+        display.textContent += target.textContent;
+
+    if (display.textContent.length > 0 && ["/", "+", "*", "-", "="].includes(target.textContent)) {
+
+                if (firstNumber.length === 0 || equalTrue === 1){
+                    firstNumber = display.textContent;
+                    equalTrue = 0;}
+            // else if (secondNumber.length === 0)
+            //     operator = target.textContent;
+            else {
+                secondNumber = display.textContent;
+                firstNumber = operate(Number(firstNumber), operator, Number(secondNumber));
+                display.textContent = firstNumber;
+                if (target.textContent === "="){
+                    secondNumber = [];
+                    operator = undefined;
+                    equalTrue = 1;}
+                }
+                
+                // if (target.textContent !== "=" && equalTrue === 1){
+                    //     operator = target.textContent;
+                    //     equalTrue = 0;
+                    // }
+                    triggerNewContent = 1;
+                    operator = target.textContent;
+    }
+
+    if (target.textContent === "=")
+
+    // if (target.textContent === "=") {
+    //     console.log(operate(Number(firstNumber), operator, Number(secondNumber)));
+    //     clearValues();
+    // }
     if (target.textContent === "CLEAR") {
         clearValues();
     }
 });
-
 
 // Test
 buttons.addEventListener("click", () => {
@@ -28,10 +55,19 @@ buttons.addEventListener("click", () => {
     )
 })
 
+let firstNumber = [];
+let secondNumber = [];
+let operator = undefined;
+display.textContent = 0;
+let triggerNewContent = 0;
+let equalTrue = 0;
+
 function clearValues() {
     firstNumber = [];
     secondNumber = [];
     operator = undefined;
+    display.textContent = 0;
+    triggerNewContent = 0;
 }
 function add(a, b) {
     return a + b;
@@ -61,4 +97,4 @@ function operate(numberOne, operator, numberTwo) {
 }
 
 // Clear values on page load
-clearValues();
+// clearValues();
